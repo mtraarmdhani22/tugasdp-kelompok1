c0dd51cd0c5d2004a714d8dfd49b350346cbbe4e
mtraarmdhani22 <ramdhanimutiara03@gmail.com>
2024-12-31 13:17:46 +0700

    Add files via upload

---
+++ dp (5).py
@@ -0,0 +1,82 @@
+# ayyarra shop
+ 
+ total = 0
+ barang = ()
+ harga = ()
+
+ while True:
+    print ("""  DAFTAR MENU AYYARA CAFE
+        seblak ceker \t 15000
+        ceker mercon \t 10000
+        baso aci pedas \t  16000
+        baso mercon \t 25000
+        es teh \t 4000
+        es teler \t 10000
+        es oyen \t 15000
+        """)
+        
+    kode = int(input('masukan kode barang'))
+    if kode = 1 :
+            barang.append('seblak ceker')
+            harga.append('15000')
+            total += harga
+    elif kode = 2 :
+        barang.append('ceker mercon')
+            harga.append('10000')
+            total += harga
+    elif kode = 3 :
+        barang.append('baso aci pedes')
+            harga.append('16000')
+            total += harga
+    elif kode = 4 :
+        barang.append('baso mercon')
+            harga.append('25000')
+            total += harga
+    elif kode = 5 :
+        barang.append('es teh')
+            harga.append('4000')
+            total += harga
+    elif kode = 6 :
+        barang.append('es teler')
+            harga.append('10000')
+            total += harga
+    elif kode = 7 :
+        barang.append('es oyen')
+            harga.append('15000')
+            total += harga
+    else :
+        print ('kode tidak valid')
+
+elif = input('lanjut mememasan (y/t) :')
+if lanjut == 't'
+print("")
+break 
+
+print ('barang yang di beli : ' ,barang)
+print ('harga barang :',harga)
+print ('total yang harus dibayar:',total,'\n')
+
+uang = int(input('masukan uang pembayaran: '))
+if uang > total :
+    print ('kembaliannya:' ,uang-total )
+elif uang = total:
+    print ('uang pas')
+else :
+    print ('uang kurang',uang-total)
+
+#strukkk
+print (""47)
+print (""47)
+print (" =====================cetak struk======================")
+print (""47)
+print ('\x1b[1;30;47m' + ' ======================== AYYARACAFE ===============' +' \ x1b [0m]')
+nomor_pesanan = int(input('order no'))
+nomor_meja = int(input('table no'))
+nomor_pembeli = int(input('costumer no'))
+nama_staff = int(input('cashier'))
+import time ;
+tanggal = time.asctime (time.localtime(time.time))
+print (' date :' ,tanggal)
+print ('_'* 47)
+print strukk 
+print ()
